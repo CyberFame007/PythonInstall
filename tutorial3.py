510bc6417231163d9a458832123d26afc40b5b13
Famous Ogbeta <dfame20+1@gmail.com>
2022-10-14 04:09:58 +0000

    m

--- tutorial3.py
+++ tutorial3.py
@@ -12,6 +12,10 @@ creation_date=s3_resource.list_buckets()["Buckets"][0]["CreationDate"]
 creation_date.strftime:("%d%m%y_%H:%M:%s")
 
 
+for bucket in s3_resource.list_buckets()["Buckets"]:
+    print(bucket["Name"])
+    print(bucket["CreationDate"])
+
 
 
 
